0622c5eadb9b9aea2fec62e7675d566db54a4cba
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-05 20:52:38 -0500

    Testing components

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,31 @@ function List(props) {
 }
 
 function App() {
-  const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  const animals = ["Lion", "Cow", "Snake", "Lizard","Goats", "Elephant", "Tiger", "Monkey", "Dog", "Cat"];
+
 
   return (
     <div>
-      <h1>Animals: </h1>
-      <List animals={animals} />
+    
+      <h1>Welcome to the Animal List</h1>
+      <p>Here is a list of animals:</p>
+      <List animals={animals} /> 
+
+   
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+      <p>Enjoy the list!</p>
+      <p>Have a great day!</p>
+      <p>Feel free to explore more!</p>
+      <p>Thank you for visiting!</p>
+      <p>Goodbye!</p>
+     
+
+
     </div>
   );
 }
