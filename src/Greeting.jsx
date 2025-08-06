c33539884b8511d77356f447513ea9ad6c77f8e0
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-05 20:59:33 -0500

    Testing components

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,8 +1,12 @@
 
-
+function Helloworld() {
+  return <h1>"Hello, World!"</h1> ;
+}
 
 function Greeting(){
    return (<div>
+    <Helloworld />;
+
 <h1>Test title</h1>
 <p>This is a test paragraph.</p>
 <p>Welcome to the Greeting component!</p>
