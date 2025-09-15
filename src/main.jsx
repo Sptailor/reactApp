bcb8549c114139c25cd496e8d80806e6cbf73f20
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-09-14 23:58:16 -0500

    testing colour useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './Person.jsx'
+
 import Greeting from "./Greeting.jsx";
 import Person from './Person.jsx';
 
